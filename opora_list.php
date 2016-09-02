1da405a0d02e15a5e41b42ec5b2213ca73c0df1f
Slava Petrov <slavik--petrov@yandex.ru>
2016-09-02 15:08:54 +0300

    Change mysql_ to mysqli_ in opora_list

--- opora_list.php
+++ opora_list.php
@@ -5,50 +5,50 @@ $user="frost";
 $pass="frost";
 $dbname="monitoringdata";
 $dbtable = "post_parameters";
-$db = mysql_connect($host,$user,$pass);
+$db = mysqli_connect($host,$user,$pass);
 
-if (!mysql_select_db($dbname)) {
+if (!mysqli_select_db($dbname)) {
     //echo "Unable to select mydbname: " . mysql_error();
     exit;
 }
 
 $query = "SET NAMES 'utf8'";
-$result=mysql_query($query);
+$result=mysqli_query($query);
 if (!$result) {
     //echo "Could not set names: " . mysql_error();
     exit;
 }
 
 $query = "SET character_set_client='utf8'";
-$result=mysql_query($query);
+$result=mysqli_query($query);
 if (!$result) {
     //echo "Could not set characterset client: " . mysql_error();
     exit;
 }
 
 $query = "SET character_set_connection='utf8'";
-$result=mysql_query($query);
+$result=mysqli_query($query);
 if (!$result) {
     //echo "Could not set characterset connection: " . mysql_error();
     exit;
 }
 
-$sql = "SELECT * FROM " . $dbtable . " WHERE baseNet='Ростов-на-Дону' ORDER BY numLine";
+$sql = "SELECT * FROM " . $dbtable . " ORDER BY numLine";
 
-$result=mysql_query($sql);
+$result=mysqli_query($sql);
 if (!$result) {
     //echo "Could not successfully run query from DB: " . mysql_error();
     exit;
 }
 
-if (mysql_num_rows($result) == 0) {
+if (mysqli_num_rows($result) == 0) {
     //echo "No rows found, nothing to print so am exiting";
     exit;
 }
 
 
 
-while ($row = mysql_fetch_assoc($result))
+while ($row = mysqli_fetch_assoc($result))
 {
 	$returned[] = array(
 					"phoneNumber" => $row['phoneNumber'],
@@ -62,8 +62,8 @@ while ($row = mysql_fetch_assoc($result))
 					);
 }
 
-mysql_free_result($result);
-mysql_close($db);
+mysqli_free_result($result);
+mysqli_close($db);
 
 
 include_once("./templates/view-opora_list.php");
