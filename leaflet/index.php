19f8698dec85ece23acc547c6b44cb0b400f3785
Slava Petrov <slavik--petrov@yandex.ru>
2016-09-13 10:28:39 +0300

    Test polyline -- fix typo

--- leaflet/index.php
+++ leaflet/index.php
@@ -22,7 +22,7 @@ var marker = [];
 var popup_text = [];
 var map = new L.Map('map');
 
-var map_layer = new L.TileLayer('../osm_tiles/${z}/${x}/${y}.png', {
+var map_layer = new L.TileLayer('../osm_tiles/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 });
